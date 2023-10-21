55765f8f209531726b837b85fa92a0c432d912ed
fzallocco <filizall97@gmail.com>
2023-10-21 18:47:30 -0400

    test2

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 export default function App() {
 
   return(
     <View style={styles.container}>
-      <Login/>
+      <Signup/>
     </View>
   );
 }
