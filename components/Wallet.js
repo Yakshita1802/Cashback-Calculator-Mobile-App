ec5eae713bbba1340e856703a77acdbaaa0822d7
fzallocco <filizall97@gmail.com>
2023-12-06 17:47:17 -0500

    dimensions test passed

--- components/Wallet.js
+++ components/Wallet.js
@@ -10,8 +10,8 @@ const Header = ({ title }) => (
   </View>
 );
 
-let deviceHeight: Dimensions.get('window').height;
-let deviceWidth: Dimensions.get('window').width;
+let deviceHeight = Dimensions.get('window').height;
+let deviceWidth = Dimensions.get('window').width;
 
 export default function Wallet({ route, navigation }) {
   const { userUID, userData } = route.params;
